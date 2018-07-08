4d285d90ddf92c14540c9ce7ca13fb23b996c07c
lichees <lichgg@163.com>
2018-07-08 17:34:05 +0800

    201878

--- pro2/pages/index/index.js
+++ pro2/pages/index/index.js
@@ -12,14 +12,19 @@ Page({
      
     },
   },
- 
+
   onLoad: function () {
    this.getMessage()
+    wx.getUserInfo({
+     success: function (res) {
+       console.log(res)
+      }
+    })
   },
   getMessage:function(){
     var obj=this;
     wx.request({
-      url: 'http://192.168.1.101/momo/index.php/Home/Index/index',
+      url: 'http://127.0.0.1/momo/index.php/Home/Index/index',
       method:'POST',
       header: {
         'content-type': 'application/x-www-form-urlencoded' // 默认值
@@ -35,21 +40,20 @@ Page({
   showdialog:function(e){
     var that = this;
     var toggleBtnVal = that.data.ahide;
-   
     var ids = e.currentTarget.dataset.ids;
-
     if (toggleBtnVal == ids) {
       that.setData({
         ahide: 0
       })
-
     }else {
       that.setData({
         ahide: ids,
-        
       })
-     
-     
     }
+  },
+  dianzan:function(e){
+    var aid = e.currentTarget.dataset.ids
+    
+
   }
 })
